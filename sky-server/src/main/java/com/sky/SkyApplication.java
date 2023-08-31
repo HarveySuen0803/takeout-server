f006e2c60a2d781ed4fbcec2ee549bb000005303
Harvey Suen <harveysuen0803@gmail.com>
2023-08-31 09:34:03 +0800

    feature (*): SpringTask config

--- sky-server/src/main/java/com/sky/SkyApplication.java
+++ sky-server/src/main/java/com/sky/SkyApplication.java
@@ -4,11 +4,13 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @Slf4j
 @EnableCaching
 @EnableTransactionManagement
+@EnableScheduling
 @SpringBootApplication
 public class SkyApplication {
     public static void main(String[] args) {
